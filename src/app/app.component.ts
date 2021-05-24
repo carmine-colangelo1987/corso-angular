e05b6ca0095338f30e10197cb40b34f344bac29f
carmine_colangelo <carmine.colangelo2@alten.it>
2021-05-24 20:01:14 +0200

    httpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+interface Post { 
+  prova: string
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'corso-angular';
+
+  constructor(private http: HttpClient) {}
+
+  onGet () {
+    this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe(
+      posts => console.log(posts),
+      err => console.log('err',err),
+      () => console.log('Complete')
+    )
+  }
+
+  onPost (postData: Post = { prova: 'Questa è una prova!' }) {
+    this.http.post(
+      'https://jsonplaceholder.typicode.com/posts',
+      postData
+      ).subscribe(
+      posts => console.log(posts),
+      err => console.log('err',err),
+      () => console.log('Complete')
+    )
+  }
+
+  onPut (postId: number = 1 , postData: Post = { prova: 'Questa è una prova!' }) {
+    this.http.put(
+      'https://jsonplaceholder.typicode.com/posts/' + postId,
+      postData
+      ).subscribe(
+      posts => console.log(posts),
+      err => console.log('err',err),
+      () => console.log('Complete')
+    )
+  }
+
+  onDelete (postId: number = 1) {
+    this.http.delete('https://jsonplaceholder.typicode.com/posts/' + postId).subscribe(
+      posts => console.log(posts),
+      err => console.log('err',err),
+      () => console.log('Complete')
+    )
+  }
 }
